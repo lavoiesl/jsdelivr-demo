37c803f42c748a32ba9d926971b093bafa3f9ada
Sébastien Lavoie <github@lavoie.sl>
2014-03-20 12:57:58 -0400

    added comments and removed the caching parts

--- deliver.php
+++ deliver.php
@@ -5,6 +5,7 @@ if (empty($_GET['f'])) {
     die('Missing file parameter.');
 }
 
+// @TODO Any logic for the concatenation
 $file = __DIR__ . '/files/' . trim($_GET['f'], './');
 
 if (!is_readable($file)) {
@@ -12,34 +13,18 @@ if (!is_readable($file)) {
     die('File not found.');
 }
 
+// Fallback for pecl_http
 if (!function_exists('http_parse_headers')) {
     require __DIR__ . '/lib/http_parse_headers.php';
 }
 
 $headers = http_parse_headers(file_get_contents($file));
 
+// @TODO Do some validation on the headers
 if ($headers === false || empty($headers['Location'])) {
     header('HTTP/1.0 404 Not Found');
     die('Invalid file.');
 }
-
-if (!empty($_SERVER['HTTP_IF_MODIFIED_SINCE']) && !empty($headers['Last-Modified'])) {
-    $file_date = strtotime($headers['Last-Modified']);
-    $request_date = strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']);
-
-    if ($file_date !== false && $request_date !== false && $file_date <= $request_date) {
-        header('HTTP/1.0 304 Not Modified');
-        die();
-    }
-}
-
-if (!empty($_SERVER['HTTP_IF_NONE_MATCH']) && !empty($headers['E-Tag'])) {
-    if ($_SERVER['HTTP_IF_NONE_MATCH'] == $headers['E-Tag']) {
-        header('HTTP/1.0 304 Not Modified');
-        die();
-    }
-}
-
 
 $location = $headers['Location'];
 unset($headers['Location']);
